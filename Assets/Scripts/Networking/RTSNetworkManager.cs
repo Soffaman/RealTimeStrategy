c100b144189fa22c780e4948eeaf0da82d17c028
Dmytro <112495732+Soffaman@users.noreply.github.com>
2024-01-10 12:19:46 +0200

    UnitSpawner formed

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
+    [SerializeField] private GameObject _unitSpawnerPrefab = null;
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
 
+        GameObject unitSpawnerInstance = Instantiate(
+            _unitSpawnerPrefab,
+            conn.identity.transform.position,
+            conn.identity.transform.rotation);
 
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
