8471ab7c76d5d8f9e4b8a0af3056cbaa2e89a275
Dmytro <112495732+Soffaman@users.noreply.github.com>
2024-01-16 16:21:00 +0200

    MultipleUnitSelecting formed

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
 
+    [SerializeField] private RectTransform _unitSelectionArea = null;
     [SerializeField] private LayerMask _layerMask = new LayerMask();
+
+    private Vector2 _startPosition;
+
+    private RTSPlayer _player;
     private Camera _mainCamera;
 
     public List<Unit> SelectedUnits { get; } = new List<Unit>();
@@ -19,36 +25,92 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void Update()
     {
+        if(_player == null)
+        {
+            _player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            foreach (Unit selectedUnit in SelectedUnits)
-            {
-                selectedUnit.Deselect();
-            }
-
-            SelectedUnits.Clear();
+            StartSelectionArea();
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             ClearSelectionArea();
         }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
+        }
+    }
+
+    private void StartSelectionArea()
+    {
+        foreach (Unit selectedUnit in SelectedUnits)
+        {
+            selectedUnit.Deselect();
+        }
+
+        SelectedUnits.Clear();
+
+        _unitSelectionArea.gameObject.SetActive(true);
+
+        _startPosition = Mouse.current.position.ReadValue();
+
+        UpdateSelectionArea();
+    }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidh = mousePosition.x - _startPosition.x;
+        float areaHeight = mousePosition.y - _startPosition.y;
+
+        _unitSelectionArea.sizeDelta = new Vector2(Mathf.Abs(areaWidh), Mathf.Abs(areaHeight));
+        _unitSelectionArea.anchoredPosition = _startPosition +
+            new Vector2(areaWidh / 2, areaHeight / 2);
     }
 
     private void ClearSelectionArea()
     {
-        Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        _unitSelectionArea.gameObject.SetActive(false);
 
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _layerMask)) { return; }
 
-        if(!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
-
-        if(!unit.isOwned) { return; }
-
-        SelectedUnits.Add(unit);
-
-        foreach (Unit selectedUnit in SelectedUnits)
+        if(_unitSelectionArea.sizeDelta.magnitude == 0)
         {
-            selectedUnit.Select();
+            Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _layerMask)) { return; }
+
+            if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
+
+            if (!unit.isOwned) { return; }
+
+            SelectedUnits.Add(unit);
+
+            foreach (Unit selectedUnit in SelectedUnits)
+            {
+                selectedUnit.Select();
+            }
+
+            return;
         }
+
+        Vector2 min = _unitSelectionArea.anchoredPosition - (_unitSelectionArea.sizeDelta / 2);
+        Vector2 max = _unitSelectionArea.anchoredPosition + (_unitSelectionArea.sizeDelta / 2);
+
+        foreach (Unit unit in _player.GetMyUnits())
+        {
+            Vector3 screenPosition = _mainCamera.WorldToScreenPoint(unit.transform.position);
+
+            if(screenPosition.x > min.x && screenPosition.x < max.x &&
+                screenPosition.y > min.y && screenPosition.y < max.y)
+            {
+                SelectedUnits.Add(unit);
+                unit.Select();
+            }
+        }
+        
     }
 }
