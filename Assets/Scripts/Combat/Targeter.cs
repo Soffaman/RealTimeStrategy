65daaf3af4e2f21bfaeb04a5dd020369b6a670e1
Dmytro <112495732+Soffaman@users.noreply.github.com>
2024-01-18 12:23:46 +0200

    Unit Chasing formed

--- Assets/Scripts/Combat/Targeter.cs
+++ Assets/Scripts/Combat/Targeter.cs
@@ -7,7 +7,10 @@ public class Targeter : NetworkBehaviour
 {
     private Targetable _target;
 
-    #region Server
+    public Targetable GetTarget()
+    {
+        return _target;
+    }
 
     [Command]
     public void CmdSetTarger(GameObject targetGameObject)
@@ -22,12 +25,4 @@ public class Targeter : NetworkBehaviour
     {
         _target = null;
     }
-
-    #endregion
-
-    #region Client
-
-
-
-    #endregion
 }
--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -7,12 +7,29 @@ public class UnitMovement : NetworkBehaviour
 {
     [SerializeField] private NavMeshAgent _agent = null;
     [SerializeField] private Targeter _targeter = null;
+    [SerializeField] private float _chaseRange = 10f;
 
     #region Server
 
     [ServerCallback]
     private void Update()
     {
+        Targetable target = _targeter.GetTarget();
+
+        if(target != null)
+        {
+            if ((target.transform.position - transform.position).sqrMagnitude > _chaseRange * _chaseRange)
+            {
+                _agent.SetDestination(target.transform.position);
+            }
+            else if (_agent.hasPath)
+            {
+                _agent.ResetPath();
+            }
+
+            return;
+        }
+
         if (!_agent.hasPath) { return; }
 
         if (_agent.remainingDistance > _agent.stoppingDistance) { return; }
