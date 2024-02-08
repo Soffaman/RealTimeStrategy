053ccc8b3fe767d49f4d68bf8832cee03fb1484a
Dmytro Podolynnyi <112495732+Soffaman@users.noreply.github.com>
2024-02-08 12:27:13 +0200

    BuildingsSetup formed

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform _mainCameraTransform;
+
+    private void Start()
+    {
+        _mainCameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + _mainCameraTransform.rotation * Vector3.forward,
+            _mainCameraTransform.rotation * Vector3.up);
+    }
+}
